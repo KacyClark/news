18b08354f78060e596798867f93c3ca5fd977678
Kacy Clark <kacy@usa.com>
2022-09-01 02:34:53 -0400

    updated

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin, 
+    UserPassesTestMixin
+)
 
-# Create your views here.
+from django.urls import reverse_lazy
+from .models import Article
+
+
+class PostListView(ListView):
+    template_name = "articles/list.html"
+    model = Article
+
+
+class PostDetailView(DetailView):
+    template_name = "articles/detail.html"
+    model = Article
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    template_name = "Articles/new.html"
+    model = Article
+    fields = ["title", "subtitle", "body", "author"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)    
+
+
+class ArticleUpdateView(
+    LoginRequiredMixin, 
+    UserPassesTestMixin, 
+    UpdateView
+):
+    template_name = "articles/edit.html"
+    model = Article
+    fields = ["title", "subtitle", "body"]
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+
+class ArticleDeleteView(
+    LoginRequiredMixin, 
+    UserPassesTestMixin, 
+    DeleteView
+):
+    template_name = "articles/delete.html"
+    model = Article
+    success_url = reverse_lazy("article_list")
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
